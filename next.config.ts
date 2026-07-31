acdb9d5fa3005697111a8ff7fec4513ca45601b9
dhandynuzirwan <169055100+dhandynuzirwan@users.noreply.github.com>
2026-07-31 14:49:29 +0700

    fix: update next.config.ts to ensure proper image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   // Biarkan kosong. Vercel & Next.js 16 sudah pintar.
 };
 
